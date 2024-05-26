e63a59458ff87239b30416f44e9efb0c12e4cc70
DesSnowy <156517235+DesSnowy@users.noreply.github.com>
2024-05-26 12:07:03 +0800

    created controllers for orders

---
+++ backend/controller/orderController.js
@@ -0,0 +1,32 @@
+//GET all orders
+const getOrders = (req, res) => {
+  res.status(200).json({ orders: "all order here" });
+};
+
+//GET a single order
+const getSingleOrder = (req, res) => {
+  res.status(200).json({ order: "single order here" });
+};
+
+//CREATE a new order
+const createOrder = (req, res) => {
+  res.status(200).json({ result: "created a new order" });
+};
+
+//DELETE an order
+const deleteOrder = (req, res) => {
+  res.status(200).json({ result: "delted an order" });
+};
+
+//UPDATE an order
+const updateOrder = (req, res) => {
+  res.status(200).json({ result: "order updated" });
+};
+
+module.exports = {
+  getOrders,
+  getSingleOrder,
+  createOrder,
+  deleteOrder,
+  updateOrder,
+};
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,29 +2,27 @@ const express = require("express");
 
 const router = express.Router();
 
+const {
+  getOrders,
+  getSingleOrder,
+  createOrder,
+  deleteOrder,
+  updateOrder,
+} = require("../controller/orderController");
+
 //GET all order
-router.get("/", () => {
-  console.log("GET all order");
-});
+router.get("/", getOrders);
 
 //get a single order
-router.get("/:id", () => {
-  console.log("GET a single order");
-});
+router.get("/:id", getSingleOrder);
 
 //POST a new order
-router.post("/", () => {
-  console.log("POST a new order");
-});
+router.post("/", createOrder);
 
 //DELETE an order
-router.delete("/:id", () => {
-  console.log("DELETE an order");
-});
+router.delete("/:id", deleteOrder);
 
 //UPDATE an order
-router.patch("/:id", () => {
-  console.log("UPDATE an order");
-});
+router.patch("/:id", updateOrder);
 
 module.exports = router;
