482c5c19bddcca4e7b15ff09e2b89d532cf70c62
DesSnowy <156517235+DesSnowy@users.noreply.github.com>
2024-05-27 19:50:06 +0800

    added mock data for api/orders/

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -1,6 +1,22 @@
 //GET all orders
 const getOrders = (req, res) => {
-  res.status(200).json({ orders: "show all order here" });
+  const orders = [
+    {
+      canteen: "frontier",
+      stall: "chinese",
+      foodItem: "chicken rice",
+      price: "3.50",
+      tele: "xyu1311",
+    },
+    {
+      canteen: "frontier",
+      stall: "korean",
+      foodItem: "some korean dish",
+      price: "3.50",
+      tele: "xyu1311",
+    },
+  ];
+  res.status(200).json(orders);
 };
 
 //GET a single order
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 //routes
-app.use("/api/order", orderRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.listen(4000, () => {
   console.log("listening on port 4000");
