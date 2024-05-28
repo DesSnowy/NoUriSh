b0cb9b5db891633c21f9bd67f5867dda528e23aa
Xiang Yu <156517235+DesSnowy@users.noreply.github.com>
2024-05-28 15:03:43 +0800

    Create database (#8)
    * added .env to gitignore and use .env
    
    * rename order to orders
    
    * added a database and modified routes to work with it
    
    * added an example .env
    
    * Create README.md
    
    * Update README.md
    
    * updated .env.example

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 const express = require("express");
-const orderRoutes = require("./routes/order");
+const orderRoutes = require("./routes/orders");
+require("dotenv").config();
 
 //express app
 const app = express();
 
 //middle ware
+
+//adding req.body to req
 app.use(express.json());
 
 //routes
 app.use("/api/orders", orderRoutes);
 
-app.listen(4000, () => {
-  console.log("listening on port 4000");
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`listening at port ${port}`);
 });
