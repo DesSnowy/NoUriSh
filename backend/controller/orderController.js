463f8d9a4af096d6a25cf219a06f265420236ce4
DesSnowy <156517235+DesSnowy@users.noreply.github.com>
2024-05-28 15:11:44 +0800

    Squashed commit of the following:
    commit b0cb9b5db891633c21f9bd67f5867dda528e23aa
    Author: Xiang Yu <156517235+DesSnowy@users.noreply.github.com>
    Date:   Tue May 28 15:03:43 2024 +0800
    
        Create database (#8)
    
        * added .env to gitignore and use .env
    
        * rename order to orders
    
        * added a database and modified routes to work with it
    
        * added an example .env
    
        * Create README.md
    
        * Update README.md
    
        * updated .env.example

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -1,42 +1,72 @@
+const db = require("../database/db");
+
 //GET all orders
-const getOrders = (req, res) => {
-  const orders = [
-    {
-      canteen: "frontier",
-      stall: "chinese",
-      foodItem: "chicken rice",
-      price: "3.50",
-      tele: "xyu1311",
-    },
-    {
-      canteen: "frontier",
-      stall: "korean",
-      foodItem: "some korean dish",
-      price: "3.50",
-      tele: "xyu1311",
-    },
-  ];
-  res.status(200).json(orders);
+const getOrders = async (req, res) => {
+  try {
+    const results = await db.query('select * from "order";');
+    res.status(200).json(results.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 };
 
 //GET a single order
-const getSingleOrder = (req, res) => {
-  res.status(200).json({ order: "single order here" });
+const getSingleOrder = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const results = await db.query('SELECT * from "order" WHERE id = $1;', [
+      id,
+    ]);
+    res.status(200).json(results.rows[0]);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 };
 
 //CREATE a new order
-const createOrder = (req, res) => {
-  res.status(200).json({ result: "created a new order" });
+const createOrder = async (req, res) => {
+  const { canteen, stall, foodItem, price, user_id } = req.body;
+  try {
+    const results = await db.query(
+      'INSERT INTO "order" (canteen, stall, fooditem, price, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [canteen, stall, foodItem, price, user_id]
+    );
+    res.status(200).json(results.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 };
 
 //DELETE an order
-const deleteOrder = (req, res) => {
-  res.status(200).json({ result: "delted an order" });
+const deleteOrder = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const results = await db.query('DELETE FROM "order" where id = $1', [id]);
+    res.status(200).json(results.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 };
 
 //UPDATE an order
-const updateOrder = (req, res) => {
-  res.status(200).json({ result: "order updated" });
+const updateOrder = async (req, res) => {
+  try {
+    const { canteen, stall, foodItem, price, user_id } = req.body;
+    const id = req.params.id;
+    const results = await db.query(
+      'UPDATE "order" SET canteen = $1, stall = $2, fooditem = $3, price = $4, user_id = $5 where id = $6 returning *',
+      [canteen, stall, foodItem, price, user_id, id]
+    );
+
+    res.status(200).json(results.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = {
