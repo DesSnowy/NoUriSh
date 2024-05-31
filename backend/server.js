ad8b7f45698233a43e37c55efcdde19d007ec4c1
DesSnowy <156517235+DesSnowy@users.noreply.github.com>
2024-06-01 00:01:57 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,30 @@ require("dotenv").config();
 const app = express();
 
 //middle ware
+
+app.use((req, res) => {
+  //set header first to allow request or origin domain (value can be different)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, OPTIONS, DELETE"
+  );
+
+  //---- other code
+
+  //Preflight CORS handler
+  if (req.method === "OPTIONS") {
+    return res.status(200).json({
+      body: "OK",
+    });
+  }
+});
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   if (req.method === "OPTIONS") {
