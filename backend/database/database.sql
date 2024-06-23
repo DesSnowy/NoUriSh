edcb7522cb87d5ac252e1bbb467ab17c0b6a9513
DesSnowy <156517235+DesSnowy@users.noreply.github.com>
2024-06-23 16:12:57 +0800

    added group relations and updated order relations

--- backend/database/database.sql
+++ backend/database/database.sql
@@ -10,15 +10,6 @@ CREATE TABLE "user" (
     residence VARCHAR(50)
 );
 
-CREATE TABLE "order" (
-    id SERIAL PRIMARY KEY,
-    canteen VARCHAR(50) NOT NULL,
-    stall VARCHAR(50) NOT NULL,
-    food_item VARCHAR(255) NOT NULL,
-    price FLOAT NOT NULL,
-    tele VARCHAR(50) NOT NULL
-);
-
 CREATE TABLE "canteen" (
     canteen_id SERIAL PRIMARY KEY,
     canteen_name VARCHAR(50),
@@ -41,6 +32,25 @@ CREATE TABLE "food" (
      CONSTRAINT fk_stall FOREIGN KEY(stall_id) REFERENCES stall(stall_id)
 );
 
+CREATE TABLE "group" (
+    group_id SERIAL PRIMARY KEY,
+    canteen_id int,
+    residence VARCHAR(50),
+    status BOOLEAN,
+    CONSTRAINT fk_canteen FOREIGN KEY(canteen_id) REFERENCES canteen(canteen_id)
+);
+
+CREATE TABLE "order" (
+    order_id SERIAL PRIMARY KEY,
+    food_id int,
+    user_email VARCHAR(255),
+    group_id int,
+    quantity int,
+    CONSTRAINT fk_food FOREIGN KEY(food_id) REFERENCES food(food_id),
+    CONSTRAINT fk_user FOREIGN KEY(user_email) REFERENCES "user"(email),
+    CONSTRAINT fk_group FOREIGN KEY(group_id) REFERENCES "group"(group_id)
+);
+
 INSERT INTO canteen VALUES
 (1, 'frontier', 'https://uci.nus.edu.sg/oca/wp-content/uploads/sites/9/2018/05/Frontier-Canteen-1024x684.jpg'),
 (2, 'deck', 'https://uci.nus.edu.sg/oca/wp-content/uploads/sites/9/2018/05/deck.jpg');
@@ -54,3 +64,7 @@ INSERT INTO stall VALUES
 INSERT INTO food VALUES
 (1, 'chicken rice', 3.5, 1, 'chicken rice description'),
 (2, 'chicken noodle',3.5, 1, 'some good noodles');
+
+INSERT INTO "group" VALUES
+(1, 1, 'PGP', true),
+(2, 2, 'Tembusu', true);
