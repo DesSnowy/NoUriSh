21b57935982341025c78cbc24097bd42390dcea5
ashleypengsy <ashleypengsy@gmail.com>
2024-06-26 17:27:55 +0800

    myorders styling

--- frontend/src/components/OrderDetails.js
+++ frontend/src/components/OrderDetails.js
@@ -9,7 +9,7 @@ const OrderDetails = ({ order }) => {
         <p className="mb-1"><span className="font-medium">Stall:</span> {order.stall}</p>
         <p className="mb-1"><span className="font-medium">Food Item:</span> {order.foodItem}</p>
         <p className="mb-1"><span className="font-medium">Quantity:</span> {order.quantity}</p>
-        <p className="mb-1"><span className="font-medium">Price:</span> ${order.price.toFixed(2)}</p>
+        <p className="mb-1"><span className="font-medium">Price:</span> ${(order.price*order.quantity).toFixed(2)}</p>
         <p className="mb-1"><span className="font-medium">Group:</span> {order.group}</p>
         <p className="mb-1"><span className="font-medium">Ordered at:</span> {new Date(order.createdAt).toLocaleString()}</p>
       </div>
