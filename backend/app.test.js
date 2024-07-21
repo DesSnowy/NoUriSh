6bb243ae8765e3efa4530fd8f48c4b7e37802fbe
DesSnowy <156517235+DesSnowy@users.noreply.github.com>
2024-07-21 17:46:37 +0800

    finish test cases for api/user

--- backend/app.test.js
+++ backend/app.test.js
@@ -11,6 +11,14 @@ const userData = {
   tele: "tester1",
   residence: "PGP",
 };
+
+const newUserData = {
+  email: "testEmail@test.com",
+  password: "test123",
+  name: "tester1New",
+  tele: "tester1New",
+  residence: "PGP",
+};
 const userToken = jwtGenerator(userData.email);
 
 describe("given unknown routes", () => {
@@ -26,16 +34,19 @@ describe("/api/user", () => {
         const { statusCode, body } = await supertest(app)
           .post("/api/user/signup")
           .send(userData);
+
         expect(statusCode).toBe(200);
         expect(body.email).toBe(userData.email);
         expect(body.token).toBeDefined();
       });
     });
+
     describe("given missing information", () => {
       it("should return 404 with {error: all field must be filled}", async () => {
         const { statusCode, body } = await supertest(app)
           .post("/api/user/signup")
           .send({});
+
         expect(statusCode).toBe(400);
         expect(body.error).toBe("all field must be filled!");
       });
@@ -48,6 +59,7 @@ describe("/api/user", () => {
         const { statusCode, body } = await supertest(app)
           .post("/api/user/login")
           .send({ email: userData.email, password: userData.password });
+
         expect(statusCode).toBe(200);
         expect(body.email).toBe(userData.email);
         expect(body.token).toBeDefined();
@@ -59,6 +71,7 @@ describe("/api/user", () => {
         const { statusCode, body } = await supertest(app)
           .post("/api/user/login")
           .send({ email: "unknownEmail", password: userData.password });
+
         expect(statusCode).toBe(400);
         expect(body.error).toBe("email does not exist!");
       });
@@ -69,6 +82,7 @@ describe("/api/user", () => {
         const { statusCode, body } = await supertest(app)
           .post("/api/user/login")
           .send({ email: userData.email, password: "wrongPassword" });
+
         expect(statusCode).toBe(400);
         expect(body.error).toBe("incorrect password!");
       });
@@ -79,6 +93,7 @@ describe("/api/user", () => {
         const { statusCode, body } = await supertest(app)
           .post("/api/user/login")
           .send({});
+
         expect(statusCode).toBe(400);
         expect(body.error).toBe("all field must be filled!");
       });
@@ -89,6 +104,7 @@ describe("/api/user", () => {
     describe("given missing authorization", () => {
       it("should return 400 with error", async () => {
         const { statusCode, body } = await supertest(app).get("/api/user");
+
         expect(statusCode).toBe(400);
         expect(body.error).toBe("Authorisation required");
       });
@@ -99,21 +115,88 @@ describe("/api/user", () => {
         const { statusCode, body } = await supertest(app)
           .get("/api/user")
           .set({ authorization: `wrongAuthorization` });
+
         expect(statusCode).toBe(400);
         expect(body.error).toBe("Request is not authorised");
       });
     });
+
     describe("given valid authorization", () => {
       it("should return the current user detail", async () => {
         const { statusCode, body } = await supertest(app)
           .get("/api/user")
           .set({ authorization: `Bearer ${userToken}` });
+
         expect(statusCode).toBe(200);
         expect(body).toStrictEqual({
           email: userData.email,
           name: userData.name,
           tele: userData.tele,
           residence: userData.residence,
+          isAdmin: false,
+        });
+      });
+    });
+  });
+
+  describe("GET /:userEmail", () => {
+    describe("given unknown useEmail", () => {
+      it("should return 400 with error", async () => {
+        const { statusCode, body } = await supertest(app)
+          .get(`/api/user/unknownEmail`)
+          .set({ authorization: `Bearer ${userToken}` });
+
+        expect(statusCode).toBe(400);
+        expect(body.error).toBe("User not found");
+      });
+    });
+
+    describe("given correct email", () => {
+      it("should return 200 with user detail", async () => {
+        const { statusCode, body } = await supertest(app)
+          .get(`/api/user/${userData.email}`)
+          .set({ authorization: `Bearer ${userToken}` });
+
+        expect(statusCode).toBe(200);
+        expect(body).toStrictEqual({
+          email: userData.email,
+          name: userData.name,
+          tele: userData.tele,
+          residence: userData.residence,
+          isAdmin: false,
+        });
+      });
+    });
+  });
+
+  describe("PATCH /", () => {
+    describe("given missing information", () => {
+      it("should return 400 with error", async () => {
+        const { statusCode, body } = await supertest(app)
+          .patch("/api/user")
+          .set({ authorization: `Bearer ${userToken}` })
+          .send({});
+        expect(statusCode).toBe(400);
+        expect(body.error).toBe("all field must be filled!");
+      });
+    });
+
+    describe("given all information", () => {
+      it("should return 200 with newUserDetail", async () => {
+        const { statusCode, body } = await supertest(app)
+          .patch("/api/user")
+          .set({ authorization: `Bearer ${userToken}` })
+          .send({
+            name: newUserData.name,
+            tele: newUserData.tele,
+            residence: newUserData.residence,
+          });
+        expect(statusCode).toBe(200);
+        expect(body).toStrictEqual({
+          email: newUserData.email,
+          name: newUserData.name,
+          tele: newUserData.tele,
+          residence: newUserData.residence,
           isAdmin: false,
         });
       });
